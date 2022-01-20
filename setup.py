9845472ea4c89c9c1fc5f0974374687558229be2
Iftah Roichman <ifttah@gmail.com>
2022-01-20 22:18:22 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gregorian-months',
+    version='1.0.0',
+    description='Python Distribution Utilities',
+    author='Iftah Roichman',
+    # author_email='gward@python.net',
+    # url='https://www.python.org/sigs/distutils-sig/',
+    packages=find_packages(),
+)
